a3a9cbc35bc272a45cc2dcf4db855cabdc9717fe
BlackScorp <witalimik@web.de>
2019-10-21 07:21:44 +0200

    new Account module

---
+++ source/Account/Module.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+namespace BlackScorp\Funci\Account;
+class Module{static function load(){}}; //this line is required for autoloading!!
+
+require_once __DIR__.'/Actions.php';
+require_once __DIR__.'/routes.php';
